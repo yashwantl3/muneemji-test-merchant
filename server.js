a3a5ce755cb1f697f426e6be773ccd6e0dd509a8
Yashwant Lalwani <yashwant.lalwani@theporter.in>
2024-05-02 15:17:46 +0530

    init

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express()
+const port = 5000
+
+app.get('/', (req, res) => {
+    res.send({
+      "status": "CAPTURE_SUCCESSFUL"
+      })
+})
+
+app.post('/', (req, res) => {
+  res.send({
+    "status": "CAPTURE_SUCCESSFUL"
+    })
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port http://localhost:${port}`)
+})
