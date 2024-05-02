cb5f052ca6594b1c63132192848bcbeef3ec2df5
Yashwant Lalwani <yashwant.lalwani@theporter.in>
2024-05-02 15:51:47 +0530

    added cors

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
-const express = require('express')
+const express = require('express') 
+const cors = require('cors')
 const app = express()
 const port = 8080
+
+app.use(cors())
 
 app.get('/', (req, res) => {
     res.send({
