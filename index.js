5bcdceab73a3b7f592089b67970252c5f9fce5ef
Yashwant Lalwani <156880229+yashwantl3@users.noreply.github.com>
2024-05-04 03:35:38 +0530

    logging response time

--- index.js
+++ index.js
@@ -1,22 +1,31 @@
-const express = require('express') 
-const cors = require('cors')
-const app = express()
-const port = process.env.PORT || 8080
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 8080;
 
-app.use(cors())
+app.use(cors());
+
+app.use((req, res, next) => {
+  const start = Date.now();
+  res.on('finish', () => {
+    const responseTime = Date.now() - start;
+    console.log(`${req.method} ${req.originalUrl} - ${res.statusCode} - ${responseTime}ms`);
+  });
+  next();
+});
 
 app.get('/', (req, res) => {
-    res.send({
-      "status": "CAPTURE_SUCCESSFUL"
-      })
-})
+  res.send({
+    "status": "CAPTURE_SUCCESSFUL"
+  });
+});
 
 app.post('/', (req, res) => {
   res.send({
     "status": "CAPTURE_SUCCESSFUL"
-    })
-})
+  });
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port http://localhost:${port}`)
-})
+  console.log(`Example app listening on port http://localhost:${port}`);
+});
