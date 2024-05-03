91c1ca15bbb790ed732bd58f19d2347fd1cdcdf6
Yashwant Lalwani <156880229+yashwantl3@users.noreply.github.com>
2024-05-03 11:50:25 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express') 
+const cors = require('cors')
+const app = express()
+const port = process.env.PORT || 8080
+
+app.use(cors())
+
+app.get('/', (req, res) => {
+    res.send({
+      "status": "CAPTURE_SUCCESSFUL"
+      })
+})
+
+app.post('/', (req, res) => {
+  res.send({
+    "status": "CAPTURE_SUCCESSFUL"
+    })
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port http://localhost:${port}`)
+})
